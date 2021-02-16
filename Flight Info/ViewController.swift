88ef661c3faad74edbae44bcfb2ee85bdd6989d8
Lucas <lucaspedrazoli@gmail.com>
2021-02-16 15:44:52 -0300

    add key frame animations for plane departure

--- Flight Info/ViewController.swift
+++ Flight Info/ViewController.swift
@@ -51,6 +51,44 @@ class ViewController: UIViewController {
   
   //MARK: custom methods
 
+  func planeDepart() {
+    let originalCenter = planeImage.center
+    UIView.animateKeyframes(withDuration: 1.5, delay: 0.0,
+      animations: {
+        UIView.addKeyframe(withRelativeStartTime: 0.0,
+                           relativeDuration:0.25) {
+            self.planeImage.center.x += 80.0
+            self.planeImage.center.y -= 10.0
+          }
+
+        UIView.addKeyframe(withRelativeStartTime: 0.1,
+                           relativeDuration: 0.4) {
+          self.planeImage.transform = CGAffineTransform(rotationAngle:-.pi / 8)
+        }
+
+        UIView.addKeyframe(withRelativeStartTime: 0.25,
+                           relativeDuration: 0.25) {
+          self.planeImage.center.x += 100.0
+          self.planeImage.center.y -= 50.0
+          self.planeImage.alpha = 0.0
+        }
+
+        UIView.addKeyframe(withRelativeStartTime: 0.51,
+                           relativeDuration: 0.01) {
+          self.planeImage.transform = .identity
+          self.planeImage.center = CGPoint(x: 0.0, y: originalCenter.y)
+        }
+
+        UIView.addKeyframe(withRelativeStartTime: 0.55,
+                           relativeDuration: 0.45) {
+          self.planeImage.alpha = 1.0
+          self.planeImage.center = originalCenter
+        }
+  },
+      completion: nil
+    )
+  }
+
   func cubeTransition(label: UILabel, text: String, direction: AnimationDirection) {
     let auxLabel = UILabel(frame: label.frame)
     auxLabel.text = text
@@ -82,6 +120,7 @@ class ViewController: UIViewController {
     summary.text = data.summary
     flightStatus.text = data.flightStatus
     if animated {
+      planeDepart()
       fade(imageView: bgImageView,
         toImage: UIImage(named: data.weatherImageName)!,
         showEffects: data.showWeatherEffects)
